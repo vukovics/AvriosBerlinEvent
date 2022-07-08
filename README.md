8e68092e65351efd05a77715ae32a8904ef1be60
Stefan Vukovic <stefanvkvc@gmail.com>
2022-07-08 16:41:14 +0200

    add readme and fix favoriteComponent

--- README.md
+++ README.md
@@ -6,22 +6,15 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
 ## Running unit tests
-
+I add some unit tests for favoriteComponent. I didn't have time to add it for all components, but you can run and try that 2 tests.
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
-## Running end-to-end tests
+## What else could be done
+  # If I had more time I would create this app to be PWA. With PWA I can add also push notifications for events. For example for favorite events, when it starts or when it ends.
+  # I would replace my storage functions with ngRx.
+  # I planed to add google maps in details dialog
+  # I'm usgin virtual scroll in this app, so the rendering performance is much better on mobile and on web.
+  # Add more tests, test for all components.
 
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
 
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/components/favorites/favorites.component.spec.ts
+++ src/app/components/favorites/favorites.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FavoritesComponent } from './favorites.component';
+import { CardComponent } from '../home/card/card.component';
+import { By } from '@angular/platform-browser';
 
 describe('FavoritesComponent', () => {
   let component: FavoritesComponent;
@@ -8,9 +9,8 @@ describe('FavoritesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FavoritesComponent ]
-    })
-    .compileComponents();
+      declarations: [FavoritesComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FavoritesComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,56 @@ describe('FavoritesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('should show 5 favorite event', () => {
+    component.favoriteEvents = [
+      {
+        rss_titel: 'test1',
+        von: '22.3.2022',
+        bon: '24.3.2022',
+        zeit: '13:00-15:00',
+      },
+      {
+        rss_titel: 'test2',
+        von: '22.3.2022',
+        bon: '24.3.2022',
+        zeit: '13:00-15:00',
+      },
+      {
+        rss_titel: 'test3',
+        von: '22.3.2022',
+        bon: '24.3.2022',
+        zeit: '13:00-15:00',
+      },
+      {
+        rss_titel: 'test4',
+        von: '22.3.2022',
+        bon: '24.3.2022',
+        zeit: '13:00-15:00',
+      },
+      {
+        rss_titel: 'test5',
+        von: '22.3.2022',
+        bon: '24.3.2022',
+        zeit: '13:00-15:00',
+      },
+    ];
+    fixture.detectChanges();
+    const cards = fixture.debugElement.queryAll(By.css('card'));
+
+    expect(cards.length).toBe(5);
+  });
+
+  fit('should show 0 cards and message if there is no favorite events', () => {
+    component.favoriteEvents = [];
+    fixture.detectChanges();
+    const cards = fixture.debugElement.queryAll(By.css('card'));
+    const message = fixture.debugElement.query(By.css('h2'));
+
+    expect(cards.length).toBe(0);
+    expect(message.nativeElement.textContent.trim()).toEqual(
+      'No favorite events :('
+    );
   });
 });
